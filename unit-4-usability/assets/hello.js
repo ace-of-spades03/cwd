62cb5aa3c0e258a11cd19fc2d52259a99d8c6b23
ace-of-spades03 <rebekahmolloy22@gmail.com>
2026-04-03 18:43:35 -0400

    made website

---
+++ unit-4-usability/assets/hello.js
@@ -0,0 +1,12 @@
+
+let myForm = document.querySelector("#myForm");
+
+myForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	console.log(this.msg.value);
+
+	//console.log("Hello");
+
+});
+
